886a6352451ddbcbd8f8b96b50fdd9ff32411758
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-02 18:50:29 +0200

    Séparateur + css billets

--- news.php
+++ news.php
@@ -11,14 +11,14 @@
     	    
     	    <div class="corps">
         	    <?php
-        	        $bdd = new PDO('mysql:host=localhost;dbname=swing', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        	        $bdd = new PDO('mysql:host=localhost;dbname=swing', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         	        $reponse = $bdd->query('SELECT id, auteur, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%i\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
         	
         	        while ($donnees = $reponse->fetch())
         	        {
         	        ?>
         	        <div class="news">
-        	            <h3> <?php echo $donnees['titre']; ?> le <em> <?php echo $donnees['date_creation_fr']; ?></em> par <?php echo $donnees['auteur']; ?></h3>
+        	            <h1> <?php echo $donnees['titre']; ?> le <em> <?php echo $donnees['date_creation_fr']; ?></em> par <?php echo $donnees['auteur']; ?></h1>
         	            <p> <?php echo $donnees['contenu']; ?> </p></br>
         	
         	        </div>
