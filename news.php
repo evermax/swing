b4d5a3c6d18ee4a372976051e6b9e1383b2c11b7
evermax <maxlasserre@free.fr>
2014-05-02 20:17:16 +0200

    News
    Finally manage to do a fine job on the news page.

--- news.php
+++ news.php
@@ -11,28 +11,26 @@
     	    
     	    <div class="corps">
         	    <?php
-        	        $bdd = new PDO('mysql:host=localhost;dbname=swing', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        	        $bdd = new PDO('mysql:host=localhost;dbname=swing', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         	        $reponse = $bdd->query('SELECT id, auteur, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%i\') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5');
         	
         	        while ($donnees = $reponse->fetch())
         	        {
         	        ?>
         	        <div class="news">
-        	            <p class="auteur" ><?php echo $donnees['auteur']; ?></p>
-        	            
-        	            <table class="billet" cellspacing="0" cellpadding="0" >
-        	                <tr>
-        	                    <th><h1> <?php echo $donnees['titre']; ?></h1></th>
-        	                </tr>
-        	                
-        	                <tr>
-        	                    <th class="contenu" > <?php echo $donnees['contenu']; ?> </th>
-        	                </tr>
-        	                
-        	                <tr>
-        	                    <th class="date" >le <em> <?php echo $donnees['date_creation_fr']; ?></em></th>
-        	                </tr>
-        	            </table>
+            	            <table class="billet" cellspacing="0" cellpadding="0" >
+            	                <tr>
+            	                    <th class="auteur"><?php echo $donnees['auteur']; ?></th><th><h1> <?php echo $donnees['titre']; ?></h1></th>
+            	                </tr>
+            	                
+            	                <tr>
+            	                    <th rowspan="2"><img src="images/singe.jpg" alt="<?php echo $donnees['auteur']; ?>"/></th><th class="contenu" > <?php echo $donnees['contenu']; ?> </th>
+            	                </tr>
+            	                
+            	                <tr>
+            	                    <th class="date" >le <em> <?php echo $donnees['date_creation_fr']; ?></em></th>
+            	                </tr>
+            	            </table>
         	
         	        </div>
         	    <?php
