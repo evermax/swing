4919357fa00df1f31df2b81b43c7816acf1d8871
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-02 20:43:59 +0200

    Page nous contacter

--- contacts.php
+++ contacts.php
@@ -10,11 +10,13 @@
             ?>
             
             <div class="corps">
+                <p>Vous avez une question ? Ce projet vous intéresse ? N'hésitez pas à nous contacter !</p>
+                
                 <h1>Contacter Fred</h1>
                 <p>Pour envoyer un mail à Fred, cliquez <a href="mailto:fred.brouzes@wanadoo.fr">ici</a>.</p>
                 
                 <h1>Contacter Paul</h1>
-                <p>Pour envoyer un mail à Paul, cliquez <a href="mailto:">ici</a>.</p>
+                <p>Pour envoyer un mail à Paul, cliquez <a href="mailto:">là</a>.</p>
             </div>
             <?php include("footer.php"); ?>
         </div>
