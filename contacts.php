b0c52bae4a4a978a5e2aa89ab7b578551c2b3b9f
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-04 19:48:01 +0200

    Suppression mail papa

--- contacts.php
+++ contacts.php
@@ -13,7 +13,7 @@
                 <p>Notre projet vous intéresse ? Vous souhaiteriez avoir des informations supplémentaires ou discuter avec l'un des inventeurs ? Vous avez une question à propos du S'Wing ? N'hésitez pas : contactez-nous par mail dès maintenant !</p>
                 
                 <h1 class="contact" >Contacter Fred</h1>
-                <p>Pour envoyer un mail à Fred, cliquez <a href="mailto:fred.brouzes@wanadoo.fr">ici</a>.</p>
+                <p>Pour envoyer un mail à Fred, cliquez <a href="mailto:">ici</a>.</p>
                 
                 <h1 class="contact" >Contacter Paul</h1>
                 <p>Pour envoyer un mail à Paul, cliquez <a href="mailto:">là</a>.</p>
