1eed9778a5d562ca3d159a29d3d6308ca721cce9
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-04 21:30:11 +0200

    Modification en-têtes toutes pages : le titre et les onglets sont alignés.

--- menus.php
+++ menus.php
@@ -1,7 +1,8 @@
 <header>
+    <h1 id="titre_principal">S'Wing</h1>
+    
     <nav>
         <ul>
-            <li><h1>S'Wing</h1></li>
             <li id="menu_index"><a href="index.php">Accueil</a></li>
             <li id="menu_description"><a href="description.php">Description</a></li>
             <li id="menu_photo"><a href="images.php">Photos et vidéos</a></li>
