d90fa093caacebf47b86add225778a70ba8c7f94
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-04 22:16:43 +0200

    Accueil : texte

--- index.php
+++ index.php
@@ -39,7 +39,8 @@
                     <article>
                         <h1>Résumé</h1>
                         <p>Bienvenu sur le site internet du S'Wing !</p>
-                        <p>Petite description.<br/>Principe dans les grandes lignes.<br/>Différentes étapes réalisées.<br/>Avancée actuelle des prototypes.<br/>Derniers résultats.</p>
+                        <p>Très bientôt, cette page contiendra une petite description du projet ainsi que le principe du S'Wing dans les grandes lignes. Vous y trouverez également les différentes étapes réalisées et l'avancée actuelle des prototypes.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum cursus lacus et risus tincidunt pulvinar. Aenean diam dolor, ornare eget scelerisque a, lacinia ut lorem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean scelerisque pretium condimentum. Etiam vitae lacus nec metus iaculis scelerisque. Curabitur nec est felis. Etiam quis tincidunt mi, ac viverra velit. Quisque eget elementum justo.</p>
                     </article>
 		
                     <aside>
