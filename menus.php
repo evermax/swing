2432b06e028245bc36b16f0f1c20f7affa96ea31
evermax <maxlasserre@free.fr>
2014-05-04 21:18:09 +0200

    Onglet actif
    Ajout d’id aux éléments du menu pour essayer de savoir sur quel onglet
    on se trouve.

--- menus.php
+++ menus.php
@@ -2,11 +2,11 @@
     <nav>
         <ul>
             <li><h1>S'Wing</h1></li>
-            <li><a href="index.php">Accueil</a></li>
-            <li><a href="description.php">Description</a></li>
-            <li><a href="images.php">Photos et vidéos</a></li>
-            <li><a href="news.php">Dernières nouvelles</a></li>
-            <li><a href="contacts.php">Contacts</a></li>
+            <li id="menu_index"><a href="index.php">Accueil</a></li>
+            <li id="menu_description"><a href="description.php">Description</a></li>
+            <li id="menu_photo"><a href="images.php">Photos et vidéos</a></li>
+            <li id="menu_news"><a href="news.php">Dernières nouvelles</a></li>
+            <li id="menu_contacts"><a href="contacts.php">Contacts</a></li>
         </ul>
     </nav>
 </header>
