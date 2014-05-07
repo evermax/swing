10d1a7e5507197224d23f261b6d66f8e715c1d26
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-08 00:10:01 +0200

    Add a section

--- contacts.php
+++ contacts.php
@@ -9,13 +9,15 @@
             ?>
             
             <div class="corps">
-                <p>Notre projet vous intéresse ? Vous souhaiteriez avoir des informations supplémentaires ou discuter avec l'un des inventeurs ? Vous avez une question à propos du S'Wing ? N'hésitez pas : contactez-nous par mail dès maintenant !</p>
-                
-                <h1 class="contact" >Contacter Fred</h1>
-                <p>Pour envoyer un mail à Fred, cliquez <a href="mailto:">ici</a>.</p>
-                
-                <h1 class="contact" >Contacter Paul</h1>
-                <p>Pour envoyer un mail à Paul, cliquez <a href="mailto:">là</a>.</p>
+                <section>
+                    <p>Notre projet vous intéresse ? Vous souhaiteriez avoir des informations supplémentaires ou discuter avec l'un des inventeurs ? Vous avez une question à propos du S'Wing ? N'hésitez pas : contactez-nous par mail dès maintenant !</p>
+                    
+                    <h1 class="contact" >Contacter Fred</h1>
+                    <p>Pour envoyer un mail à Fred, cliquez <a href="mailto:">ici</a>.</p>
+                    
+                    <h1 class="contact" >Contacter Paul</h1>
+                    <p>Pour envoyer un mail à Paul, cliquez <a href="mailto:">là</a>.</p>
+                </section>
             </div>
             <?php include("footer.php"); ?>
         </div>
