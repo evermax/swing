5056ee0c72565586207938458cbe9ecabc279778
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-08 13:20:50 +0200

    Nouvelle page admin : ajouter et supprimer des images dans le carousel. Vide pour l'instant.

---
+++ gestion_images.php
@@ -0,0 +1,49 @@
+<?php session_start();?>
+
+<!DOCTYPE html>
+<html>
+	<?php include("head.php"); ?>
+
+    <body>
+    <?php
+    	if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+    	{
+    		$pseudo = $_SESSION['pseudo'];
+    		?>
+    		<div class="bloc_page">
+    		    <?php
+                    include("menu_admin.php");
+                ?>
+    		    
+    		    <div class="corps">
+        		    <form action="ajout_image_post.php" method="post">
+        		        <table>
+        		            <tr>
+        		                <td><label for="titre">Titre : </label></td>
+        		                <td><input name="titre" id="titre" /></td>
+        		            </tr>
+        		            
+        		            <tr>
+        		                <td><label for="titre">Titre : </label></td>
+        		                <td><input name="titre" id="titre" /></td>
+        		            </tr>
+        		            
+            		    	<tr>
+            		    	    <td colspan="2" ><input type="submit" value="Envoyer" /></td>
+                            </tr>
+        		        </table>
+        		    </form>
+    		    </div>
+    		    <?php include("footer.php"); ?>
+    		</div> 
+			<?php
+    	}
+    	else
+    	{
+		    ?>
+		    perdu ?
+		    <?php
+    }
+    ?>
+    </body>
+</html>
