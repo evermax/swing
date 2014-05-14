708d9f835d301a5e4688e3b03f46738573e326eb
Maxime <maxlasserre@free.fr>
2014-05-14 21:25:25 +0200

    Erreur

---
+++ gestion_images.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<?php include("head.php"); ?>
+
+    <body>
+        <div class="bloc_page">
+            <?php
+            	if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+            	{
+            		$pseudo = $_SESSION['pseudo'];
+            		include("menu_admin.php");
+            		?>
+            		<h1>Gérer les images</h1>
+            <?php } else { ?>
+            Perdu ?
+            <?php } ?>
+        </div>
+    </body>
+</html>
