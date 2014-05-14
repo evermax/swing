82bda833f7820ab13f1827614ba7bf6786e487b0
Maxime <maxlasserre@free.fr>
2014-05-14 20:19:38 +0200

    Sécurisation de l'envoi des images
    Il manque maintenant à les renommer suivant la base de données

--- upload.php
+++ upload.php
@@ -2,17 +2,24 @@
 
     $error    = NULL;
     $filename = NULL;
+    $filetype = NULL;
 
     if (isset($_FILES['uploadFile']) && $_FILES['uploadFile']['error'] === 0) {
 
         $filename = $_FILES['uploadFile']['name'];
-        $targetpath = getcwd() . '/images/' . $filename; // On stocke le chemin où enregistrer le fichier
+        $filetype = $_FILES['uploadFile']['type'];
+        $targetpath = getcwd() . '/images/carousel_images/' . $filename; // On stocke le chemin où enregistrer le fichier
         
-        // On déplace le fichier depuis le répertoire temporaire vers $targetpath
-        if (@move_uploaded_file($_FILES['uploadFile']['tmp_name'], $targetpath)) { // Si ça fonctionne
-            $error = 'OK';
-        } else { // Si ça ne fonctionne pas
-            $error = "Échec de l'enregistrement !";
+        
+        if (explode("/", $filetype)[0] ==='image') {
+            // On déplace le fichier depuis le répertoire temporaire vers $targetpath
+            if (@move_uploaded_file($_FILES['uploadFile']['tmp_name'], $targetpath)) { // Si ça fonctionne
+                $error = 'OK';
+            } else { // Si ça ne fonctionne pas
+                $error = "Échec de l'enregistrement !";
+            }
+        } else {
+            $error = 'Il faut une image !';
         }
     } else {
         $error = 'Aucun fichier réceptionné !';
