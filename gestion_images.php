7a3863d13a91f0c974d116ef73fbf6f454edc34d
Maxime <maxlasserre@free.fr>
2014-05-14 21:50:12 +0200

    Tentative de gestion de la suppression des images

--- gestion_images.php
+++ gestion_images.php
@@ -1,25 +1,44 @@
+<?php session_start();?>
+
 <!DOCTYPE html>
 <html>
 	<?php include("head.php"); ?>
 
     <body>
-        <div class="bloc_page">
-            <?php
-            	if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
-            	{
-            		$pseudo = $_SESSION['pseudo'];
-            		include("menu_admin.php");
-            		?>
-            		<h1>Gérer les images</h1>
-            <?php
-                }
-                else
-                {
-            ?>
-            Perdu ?
-            <?php
-                }
-            ?>
+    <?php
+    	if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+    	{
+    		$pseudo = $_SESSION['pseudo'];
+    		?>
+    		<div class="bloc_page">
+    		    <?php
+                    include("menu_admin.php");
+                    $bdd = new PDO('mysql:host=localhost;dbname=swing', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                    $reponse = $bdd->query('SELECT * FROM legende_carousel ORDER BY id');
+                ?>
+                <h1>Gestion images</h1>
+                
+                <?php
+                    while($donnees = $reponse->fetch())
+                    {
+    		            ?>
+    			        <a href="supprimer_photo.php?photo=<?php echo($donnees['nom']); ?>" rel="nofollow" ><img src="images/carousel_images/<?php echo($donnees['nom']); ?>" alt="images/carousel_images/<?php echo($donnees['nom']); ?>" /></a>
+    			        <?php
+                    }
+                ?>
+                
+                
+    		    <?php include("footer.php"); ?>
+    		</div> 
+			<?php
+    	}
+    	else
+    	{
+		    ?>
+		    perdu ?
+		    <?php
+    }
+    ?>
         </div>
     </body>
 </html>
---
+++ supprimer_photo.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+{
+	$nom = strip_tags($_GET['photo']);
+	
+	$bdd = new PDO('mysql:host=localhost;dbname=swing', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$requete = $bdd->prepare('DELETE FROM legende_carousel WHERE nom = :nom');
+	$requete->execute(array('nom' => $nom));
+	
+	header('Location: gestion_images.php');
+}
+?>
