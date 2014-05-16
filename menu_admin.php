81cc41ff24470ba30e146c48e003cb021f55fc48
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-16 13:32:10 +0200

    Ajout du logo partie admin.

--- menu_admin.php
+++ menu_admin.php
@@ -1,15 +1,16 @@
 <header>
-    <h1 class="titre_principal">S'Wing</h1>
+    <div class="titre_principal">
+        <img src="images/icones/s-wing.png" id="logo" alt="S'Wing">
+    </div>
+    
     <nav>
-        <div class="menu">
-            <ul class="onglets">
-                <li id="ajout_billet"><a href="ajout_billet.php">Ajout billet</a></li>
-                <li id="gestion_billets"><a href="gestion_billets.php">Gestion billets</a></li>
-                <li id="ajout_image"><a href="ajout_image.php">Ajout image</a></li>
-                <li id="gestion_images"><a href="gestion_images.php">Gestion images</a></li>
-                <li id="changer_mdp"><a href="changer_mdp.php">Changement MDP</a></li>
-            </ul>
-        </div>
+        <ul class="onglets">
+            <li id="ajout_billet"><a href="ajout_billet.php">Ajout billet</a></li>
+            <li id="gestion_billets"><a href="gestion_billets.php">Gestion billets</a></li>
+            <li id="ajout_image"><a href="ajout_image.php">Ajout image</a></li>
+            <li id="gestion_images"><a href="gestion_images.php">Gestion images</a></li>
+            <li id="changer_mdp"><a href="changer_mdp.php">Changement MDP</a></li>
+        </ul>
     </nav>
 </header>
 <script src="onglet_admin.js"></script>
