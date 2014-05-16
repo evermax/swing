9588fad3e1f9522c344c7ed8f86523477d3bf6e8
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-16 11:09:57 +0200

    Ajout du logo.

--- menus.php
+++ menus.php
@@ -1,16 +1,17 @@
 <header>
-    <a href="index.php"><h1 class="titre_principal">S'Wing</h1></a>
+    <div class="titre_principal">
+        <a href="index.php" ><img src="images/icones/s-wing.png" id="logo" alt="S'Wing"></a>
+    </div>
     
     <nav>
-        <div class="menu">
-            <ul class="onglets">
-                <li id="menu_index"><a href="index.php">Accueil</a></li>
-                <li id="menu_description"><a href="description.php">Description</a></li>
-                <li id="menu_photo"><a href="images.php">Photos et vidéos</a></li>
-                <li id="menu_news"><a href="news.php">Dernières nouvelles</a></li>
-                <li id="menu_contacts"><a href="contacts.php">Contacts</a></li>
-            </ul>
-        </div>
+        <ul class="onglets">
+            <li id="menu_index"><a href="index.php">Accueil</a></li>
+            <li id="menu_description"><a href="description.php">Description</a></li>
+            <li id="menu_photo"><a href="images.php">Photos et vidéos</a></li>
+            <li id="menu_news"><a href="news.php">Dernières nouvelles</a></li>
+            <li id="menu_contacts"><a href="contacts.php">Contacts</a></li>
+        </ul>
     </nav>
 </header>
 <script src="onglet.js"></script>
+
