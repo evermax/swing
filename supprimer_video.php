1c0a4aa3e6dca6b576cdd1c78f1eb53e3583a844
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-18 10:39:56 +0200

    Suppression vidéo.

--- supprimer_video.php
+++ supprimer_video.php
@@ -6,24 +6,24 @@ if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
 	$nom = strip_tags($_GET['video']);
 	
 	include("base.inc.php");
+	$requete = $bdd->prepare('DELETE FROM video WHERE nom = :nom');
+	$requete->execute(array('nom' => $nom));
 		
 	unlink ('images/videos/' . $nom);
 
-	$reponse = $bdd->prepare('SELECT formate FROM video WHERE nom = :nom');
+	$req = $bdd->prepare('SELECT formate FROM video WHERE nom = :nom');
+	$req->execute(array('nom' => $nom));
 	$donnees = $reponse->fetch();
 	
 	if ($donnees['formate'] !== '0')
     {
-	    $nom_exp = explode(".", $donnees['nom'])[0];
+	    $nom_exp = explode(".", $nom)[0];
 	
 	    unlink ('images/videos/' . $nom_exp . '.mp4');
 	    unlink ('images/videos/' . $nom_exp . '.webm');
 	    unlink ('images/videos/' . $nom_exp . '.oggtheora.ogv');
 	    unlink ('videos/' . $nom_exp . '.html');
 	}
-	
-	$requete = $bdd->prepare('DELETE FROM video WHERE nom = :nom');
-	$requete->execute(array('nom' => $nom));
 	
 	header('Location: gestion_images.php?modification=supprimer');
 }
