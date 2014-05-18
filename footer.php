dddf14128629e3d727f0831f699ec343a9aa0996
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-18 16:39:42 +0200

    Texte au survol du bouton pour remonter au haut de la page.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer>
-    <div class="backup" onclick="window.scrollTo(0, 0)"></div>
+    <div class="backup" onclick="window.scrollTo(0, 0)" title="Haut de la page"></div>
     <p>Copyright © <strong>Chloé Brouzes & Maxime Lasserre</strong></p>
 </footer>
