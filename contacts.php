5afad52d83380b463ac7cbc9fad7e381ad2fa407
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-18 15:01:08 +0200

    Correction texte Contacts.

--- contacts.php
+++ contacts.php
@@ -10,7 +10,7 @@
             
             <div class="corps">
                 <section>
-                    <p>Notre projet vous intéresse ? Vous souhaiteriez avoir des informations supplémentaires ou discuter avec l'un des inventeurs ? Vous avez une question à propos du S'Wing ? N'hésitez pas : contactez-nous par mail dès maintenant !</p>
+                    <p>Notre projet vous intéresse ? Vous souhaiteriez avoir des informations supplémentaires ou discuter avec l'un des inventeurs ? Vous avez une question à propos de la S'Wing ? N'hésitez pas : contactez-nous par mail dès maintenant !</p>
                     
                     <h2>Contacter Fred</h2>
                     <p>Pour envoyer un mail à Fred, cliquez <a href="mailto:fwifred@hotmail.fr?subject=S'Wing">ici</a>.</p>
