2c66193290b40207185f27ce813b3ca35672ba7b
Chloebrz <chloe.brouzes@hotmail.fr>
2014-05-31 21:22:59 +0200

    Modification des quelques lignes dépot de brevet Triul.

--- historique.php
+++ historique.php
@@ -12,7 +12,7 @@
                 <section>
                     <h1>S'Wing et antécédents</h1>
                     
-                    <p>La S'Wing est l'aboutissement de plusieurs projets destinés à éliminer ou réduire les inconvénients des voiliers actuels grâce à l'auto-orientation de la voilure. Le concept utilisé sur le <strong>Triul</strong>, projet précédent, était dérivé de l'équilibre aérodynamique des avions et planeurs tel que défini par Voisin : "Centre de gravité en avant du centre de voilure, et empennage horizontal arrière inversé", (transposé à 90 degrés pour le Triul).</p>
+                    <p>La S'Wing est l'<strong>aboutissement de plusieurs projets</strong> destinés à éliminer ou réduire les inconvénients des voiliers actuels grâce à l'<strong>auto-orientation de la voilure</strong>. Le concept utilisé sur le <strong>Triul</strong>, projet précédent, était dérivé de l'équilibre aérodynamique des avions et planeurs tel que défini par Voisin : "Centre de gravité en avant du centre de voilure, et empennage horizontal arrière inversé", (transposé à 90 degrés pour le Triul).</p>
                     <p>La S'Wing utilise les conditions d'équilibre aérodynamique du parapente, transposées du plan vertical au plan horizontal. Cette option permet d'alléger le dispositif en remplaçant la voilure de type Marconi par des voiles plus légères et le bateau par une petite nacelle indépendante de la voilure.</p>
                 </section>
                 
@@ -23,8 +23,7 @@
                     
                     <p><img src="images/historique/Triul 4 theorie.jpg" alt="Théorie du Triul" /></p>
                     
-                    <p>Dépôt de brevet du Triul.
-                    <a href="images/historique/Triul 5 Ptc2.jpg">ici</a> ou <a href="images/historique/Triul 6 Ptc.jpg">là</a></p>
+                    <p>Ce projet a fait l'objet d'un <strong>dépôt de brevet</strong>, le 29 octobre 1992.<br />Pour accéder à ces documents, cliquez <a href="images/historique/Triul 5 Ptc2.jpg">ici</a> ou <a href="images/historique/Triul 6 Ptc.jpg">là</a>.</p>
                 </section>
                 
                 <p></p>
