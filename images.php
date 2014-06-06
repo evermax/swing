eb0721727eb14f7665edfd7a9b51de4d71a7ded9
Chloebrz <chloe.brouzes@hotmail.fr>
2014-06-06 23:47:32 +0200

    Inversion images et vidéos. Changement de l'image fixe dans vidéos.

--- images.php
+++ images.php
@@ -14,6 +14,54 @@
                 include("base.inc.php");
                 $reponse = $bdd->query('SELECT * FROM legende_carousel ORDER BY id');
                 ?>
+                
+                <section>
+				    <h1>Vidéos</h1>
+				
+				    <div id="contenu_video">
+				        <img src="images/videos/img6.jpg" alt="VIDEOS" />
+				    </div>
+				
+				    <div class="div_videos">
+				    <p class="center"><strong>Cliquer sur l'aperçu d'une vidéo pour la regarder !</strong></p>
+				    <?php
+				        $rep = $bdd->query('SELECT * FROM video WHERE formate=1 ORDER BY id');
+				        while($donnees = $rep->fetch())
+                        {
+				            $nom = explode(".", $donnees['nom'])[0] ?>
+				            
+				            <p>
+				            <table>
+				                <tr>
+				                    <td>
+				                        <div class="miniature">
+				                            <a href="videos/<?php echo $nom ?>.html" class="liens" title="Cliquez pour regarder la vidéo !"><img src="images/videos/<?php echo $nom ?>.png" alt="<?php echo $nom ?>" /></a>
+				                        </div>
+				                    </td>
+				                    
+				                    <td>
+                                        <table class="table_videos" cellspacing="0" cellpadding="0" >
+                        	                <tr>
+                        	                    <td class="titre_video">
+                        	                        <?php echo $donnees['titre'] ?>
+                        	                    </td>
+                        	                </tr>
+                        	                
+                        	                <tr>
+                        	                    <td class="bottom" ><?php echo $donnees['legende']; ?></td>
+                        	                </tr>
+                        	            </table>
+                    	            </td>
+                    	        </tr>
+                	        </table>
+            	            </p>
+                            <?php
+                        }
+                    ?>
+                    </div>
+                </section>
+                
+                <p></p>
 
                 <section>
                     <h1>Images</h1>
@@ -67,54 +115,6 @@
 			        </div>
 			        <!-- // end of photosgallery-std -->
 			    </section>
-				
-				<p></p>
-				
-				<section>
-				    <h1>Vidéos</h1>
-				
-				    <div id="contenu_video">
-				        <img src="images/videos/video.png" alt="VIDEOS" />
-				    </div>
-				
-				    <div class="div_videos">
-				    <p class="center"><strong>Cliquer sur l'aperçu de la vidéo pour la regarder !</strong></p>
-				    <?php
-				        $rep = $bdd->query('SELECT * FROM video WHERE formate=1 ORDER BY id');
-				        while($donnees = $rep->fetch())
-                        {
-				            $nom = explode(".", $donnees['nom'])[0] ?>
-				            
-				            <p>
-				            <table>
-				                <tr>
-				                    <td>
-				                        <div class="miniature">
-				                            <a href="videos/<?php echo $nom ?>.html" class="liens" title="Cliquez pour regarder la vidéo !"><img src="images/videos/<?php echo $nom ?>.png" alt="<?php echo $nom ?>" /></a>
-				                        </div>
-				                    </td>
-				                    
-				                    <td>
-                                        <table class="table_videos" cellspacing="0" cellpadding="0" >
-                        	                <tr>
-                        	                    <td class="titre_video">
-                        	                        <?php echo $donnees['titre'] ?>
-                        	                    </td>
-                        	                </tr>
-                        	                
-                        	                <tr>
-                        	                    <td class="bottom" ><?php echo $donnees['legende']; ?></td>
-                        	                </tr>
-                        	            </table>
-                    	            </td>
-                    	        </tr>
-                	        </table>
-            	            </p>
-                            <?php
-                        }
-                    ?>
-                    </div>
-                </section>
 
             </div>
             <?php include("footer.php"); ?>
