82d8b1bf77b2cf3dbec8ea804028710e35a4e9dd
Chloebrz <chloe.brouzes@hotmail.fr>
2014-06-16 18:33:25 +0200

    Renommage de la vidéo du triul et ajout de la vidéo dans la page. L'ajouter aux bons formats avec la musique.

---
+++ triul.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+	<?php include("head.php"); ?>
+
+    <body>
+        <div class="bloc_page">
+            <?php
+            include("menus.php");
+            ?>
+            
+            <div class="corps">
+                <section>
+                    <h1>S'Wing et antécédents</h1>
+                    
+                    <p>La S'Wing est l'<strong>aboutissement de plusieurs projets</strong> destinés à éliminer ou réduire les inconvénients des voiliers actuels grâce à l'<strong>auto-orientation de la voilure</strong>. Le concept utilisé sur le <strong>Triul</strong>, projet précédent, était dérivé de l'équilibre aérodynamique des avions et planeurs tel que défini par Voisin : "Centre de gravité en avant du centre de voilure, et empennage horizontal arrière inversé", (transposé à 90 degrés pour le Triul).</p>
+                    <p>La S'Wing utilise les conditions d'équilibre aérodynamique du parapente, transposées du plan vertical au plan horizontal. Cette option permet d'alléger le dispositif en remplaçant la voilure de type Marconi par des voiles plus légères et le bateau par une petite nacelle indépendante de la voilure.</p>
+                </section>
+                
+                <p></p>
+                
+                <section>
+                    <h1>Le Triul : théorie et prototypes</h1>
+                    
+                    <p><img src="images/triul/Sit Triul.png" alt="Théorie et prototype du Triul" /></p>
+                </section>
+                
+                <p></p>
+                
+                <section>
+                    <h1>Le Triul : démonstration en vidéo</h1>
+                    
+                    <video width="600" controls="">
+                        <source src="images/triul/video_triul.mp4"></source>
+                        <source src="images/triul/video_triul.webm"></source>
+                        <source src="images/triul/video_triul.oggtheora.ogv"></source>
+                    <video/>
+                </section>
+                
+                <p></p>
+                
+                <section>
+                    <h1>Le Triul : publications</h1>
+                    
+                    <p><img src="images/triul/Sit publications.png" alt="Publications Triul" /></p>
+                </section>
+                
+                <p></p>
+                
+                <section>
+                    <h1>Le Triul : brevet</h1>
+                    
+                    <p><img src="images/triul/Sit Brev.png" alt="Brevet Triul" /></p>
+                    
+                    <p>Ce projet a fait l'objet d'un <strong>dépôt de brevet</strong>, le 29 octobre 1992.<br />Pour accéder à ces documents, cliquez <a href="images/triul/Triul 5 Ptc2.jpg">ici</a> ou <a href="images/historique/Triul 6 Pct.jpg">là</a>.</p>
+                </section>
+            </div>
+            <?php include("footer.php"); ?>
+        </div>
+    </body>
+</html>
